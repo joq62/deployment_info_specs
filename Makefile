98dd686ff48d19aa42ef18637fe1f21ee7548f6b
joq62 <joakim.leche@gmail.com>
2022-05-22 21:07:17 +0200

    update

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 all:
-	rm -rf *~
+	rm -rf *~ *.beam *_crash.dump
+check:
+	rm -rf *~ *.beam *_crash.dump;
+	erlc *.erl;
+	erl -run deployment_check start
 git:
-	rm -rf *~;
+	rm -rf *~ *_crash.dump  *.beam ;
 	git add *;
 	git commit -m update;
-	git push https://joq62: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@github.com/joq62/deployment_info_spec.git --all;
+	git push https://joq62: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@github.com/joq62/deployment_info_specs.git --all;
 	echo hello and done!
---
+++ deployment_check.erl
@@ -0,0 +1,55 @@
+-module(deployment_check).
+
+-export([
+	 start/0,
+	 %% Support
+	 all_files/0,
+	 all_info/0
+	]).
+
+
+-define(DirSpecs,".").
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+start()->
+    
+    check(all_info()),
+    init:stop(),
+    ok.
+
+check([])->
+    io:format("Ok, no errors!! ~n");
+check([L|T])->
+    io:format("Checking ~p~n",[L]),
+    true=proplists:is_defined(deployment_id,L),
+    true=proplists:is_defined(deployment_vsn,L),
+    true=proplists:is_defined(appl_id,L),
+    true=proplists:is_defined(appl_vsn,L),
+    true=proplists:is_defined(nodes,L),
+    check(T).
+
+   
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+all_files()->
+    {ok,Files}=file:list_dir(?DirSpecs),
+    DeplFiles=[filename:join(?DirSpecs,Filename)||Filename<-Files,
+							    ".depl"=:=filename:extension(Filename)],
+    DeplFiles.    
+all_info()->
+    L1=[file:consult(DeplFile)||DeplFile<-all_files()],
+%    io:format(" L1 ~p~n",[L1]),
+    [Info||{ok,Info}<-L1].
+
+
+%find(DeplId)->
+    
+    
